a470f9bece36e765b221e86c061032d8d43f7e5f
User <nathansilva50@gmail.com>
2018-06-06 16:30:27 -0700

    will continue soon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+import { noUndefined } from '@angular/compiler/src/util';
+
+class Person {
+  name: string;
+  gender: string;
+  rating: number;
+
+  constructor(name: string, gender: string, rating: number) {
+    this.name = name;
+    this.gender = gender;
+    this.rating = rating;
+  }
+  
+}
+
+const male = new Person('adam', 'male', 0);
+const female = new Person('eve', 'female', 0);
+const jonBoy = new Person('jonny', 'apache helicopter', -75);
 
 @Component({
   selector: 'app-root',
@@ -7,4 +25,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  
+  person= this.naturalSelection();
+
+  naturalSelection() {
+    let fate = Math.floor(Math.random() * 3);
+    if(fate === 0){
+      return male;
+    } else if(fate === 1){
+      return female;
+    } else if(fate === 2){
+      return jonBoy;
+    }
+  }
 }
